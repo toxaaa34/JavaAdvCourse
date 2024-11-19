6aa5879eb69e4c7d706cb89cbeffdc5028b58ad0
toxaaa34 <aaa34@vk.com>
2024-11-19 13:10:10 +0300

    fix

--- src/main/java/org/example/DayTwo.java
+++ src/main/java/org/example/DayTwo.java
@@ -5,45 +5,23 @@ public class DayTwo {
         example();
     }
 
+    public static void work(int n, int x) {
+        for (int i = 1; i <= n; i++) {
+            try {
+                Thread.sleep(x);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println((int)((i /(double)n) * 100) + " %");
+            System.out.println(Thread.currentThread());
+        }
+        System.out.println("конец");
+    }
+
     private static void example() {
-        Thread t1 = new Thread(() -> {
-            for (int i = 1; i <= 10; i++) {
-                try {
-                    Thread.sleep(4000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println((int)((i/10d) * 100) + " %");
-                System.out.println(Thread.currentThread());
-            }
-            System.out.println("конец");
-        }, "поток t1");
-
-        Thread t2 = new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println((int)((i/5d) * 100) + " %");
-                System.out.println(Thread.currentThread());
-            }
-            System.out.println("конец");
-        }, "поток t2");
-
-        Thread t3 = new Thread(() -> {
-            for (int i = 1; i <= 7; i++) {
-                try {
-                    Thread.sleep(4000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println((int)((i/7d) * 100) + " %");
-                System.out.println(Thread.currentThread());
-            }
-            System.out.println("конец");
-        }, "поток t3");
+        Thread t1 = new Thread(() ->  work(10, 4000), "поток t1");
+        Thread t2 = new Thread(() ->  work(5, 7000), "поток t2");
+        Thread t3 = new Thread(() ->  work(17, 2000), "поток t3");
 
         t1.start();
         t2.start();
